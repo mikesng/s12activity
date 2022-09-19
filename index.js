bbd4ed2db01e011d155eb53516e38ffe20b958bb
mikesng <113398372+mikesng@users.noreply.github.com>
2022-09-19 15:44:13 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+
+let firstName = "John";
+console.log("First Name: " + firstName);
+
+let lastName = "Smith";
+console.log("Last Name: " + lastName);
+
+let age = 30;
+console.log("Age: " + age);
+
+console.log("Hobbies: ");
+
+let hobbies = ["Biking","Mountain Climbing","Swimming"]
+console.log(hobbies);
+
+console.log("Work Address:	");
+
+let workAddress = {
+	houseNumber: "32",
+	street: "Washington",
+	city: "Lincoln",
+	state: "Nebraska"
+};
+
+console.log(workAddress);
+
+let fullName = "Steve Rogers";
+console.log("My full name is: " + fullName);
+
+let currentAge = 40;
+console.log("My current age is: " + currentAge);
+
+console.log("My Friends are: ");
+
+let friends = ["Tony","Bruce", "Thor", "Natasha", "Clint", "Nick"]
+console.log(friends);
+
+console.log("My Full Profile: ");
+
+let fullProfile = {
+	username: "captain_america",
+	fullName: "Steve Rogers",
+	age: + currentAge,
+	isActive: false
+};
+
+console.log(fullProfile);
+
+let bestFriend = "Bucky Barnes";
+console.log("My bestfriend is: " + bestFriend);
+
+let loc = "Arctic Ocean";
+console.log("I was found frozen in: " + loc);
